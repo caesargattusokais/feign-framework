834768c7cfad9e64d160d726af8b1d0da5162803
wdz <15349812798@163.com>
2026-06-14 00:17:38 +0800

    删除不必要代码

--- core/src/main/java/com/feign/framework/FeignClient.java
+++
@@ -1,8 +0,0 @@
-package com.feign.framework;
-
-import com.feign.framework.Response;
-import com.feign.framework.http.Request;
-
-public interface FeignClient {
-    Response execute(Request request);
-}
